692311594434f41a16a3b1c925fd7d96dbffae7d
Vermont Garcia <vermontgarcia@gmail.com>
2025-05-10 13:38:30 -0600

    Expose to LAN

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  optimizeDeps: {
+    exclude: ['@xenova/seamless-m4t'], // Needed for WebAssembly
+  },
+  server: {
+    host: '0.0.0.0',
+    port: 5173,
+    headers: {
+      'Cross-Origin-Opener-Policy': 'same-origin',
+      'Cross-Origin-Embedder-Policy': 'require-corp',
+    },
+  },
+});
